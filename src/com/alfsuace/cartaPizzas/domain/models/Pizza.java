cc328104efdfebd0e5dbd7675c7d3e60e797903c
alfsuace <alfonso.suaace@educa.jcyl.es>
2023-02-06 17:27:26 +0100

    implementacion de Pizza

---
+++ src/com/alfsuace/cartaPizzas/domain/models/Pizza.java
@@ -0,0 +1,43 @@
+package com.alfsuace.cartaPizzas.domain.models;
+
+public class Pizza {
+
+    private String id;
+    private String nombre;
+    private String salsa;
+    private String extras;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSalsa() {
+        return salsa;
+    }
+
+    public void setSalsa(String salsa) {
+        this.salsa = salsa;
+    }
+
+    public String getExtras() {
+        return extras;
+    }
+
+    public void setExtras(String extras) {
+        this.extras = extras;
+    }
+
+}
